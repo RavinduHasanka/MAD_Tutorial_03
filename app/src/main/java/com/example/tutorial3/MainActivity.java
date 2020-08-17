0334d74501fa387fc287db4c4cf6e15c56dd48d7
Ravindu Hasanka <it19156798@my.sliit.lk>
2020-08-17 21:35:04 +0530

    Add MainActivity and Customer_Tost

---
+++ app/src/main/java/com/example/tutorial3/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.tutorial3;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public static final String EXTRA_MESSAGE ="com.example.myfirstapp.MESSAGE";
+
+
+    /** Called when the user taps the Send button */
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, CustomToast.class);
+        EditText editText = (EditText) findViewById(R.id.editTextTextPersonName);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+
+}
